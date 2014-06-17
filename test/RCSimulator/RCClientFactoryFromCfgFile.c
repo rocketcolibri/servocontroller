8f3308431778e0c5507222dbc9e2a5f2db89be1e
schelor0 <schelor0@ulafpc19>
2014-06-17 08:03:31 +0200

    fixed ReadCfgFile method

--- test/RCSimulator/RCClientFactoryFromCfgFile.c
+++ test/RCSimulator/RCClientFactoryFromCfgFile.c
@@ -11,6 +11,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <strings.h>
+#include <errno.h>
 #include <memory.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -29,14 +30,25 @@
 
 static char *ReadCfgFile(const char *pConfigFileName, UINT32 *pSize)
 {
-	FILE *pCfgFile = fopen(pConfigFileName, "r");
-
-	// TODO
-	char *pBuffer = malloc(100000);
-	 size_t s = fread(pBuffer,  1, 100000, pCfgFile);
-	 *pSize = s;
-	fclose(pCfgFile);
-	return pBuffer;
+	char *buffer=NULL;
+	UINT32 numbytes=0;
+	FILE *infile = fopen(pConfigFileName, "r");
+	if(infile)
+	{
+		fseek(infile, 0L, SEEK_END); //Get the number of bytes
+		numbytes = ftell(infile);
+		fseek(infile, 0L, SEEK_SET); // reset the file position indicator to the beginning of the file
+		buffer = (char*)calloc(numbytes, sizeof(char));
+		DBG_ASSERT(buffer);
+		fread(buffer, sizeof(char), numbytes, infile);
+		fclose(infile);
+	}
+	else
+	{
+		DBG_MAKE_ENTRY_MSG(TRUE, strerror(errno));
+	}
+	*pSize = numbytes;
+	return buffer;
 }
 
 void RCClientFactoryFromCfgFile(const char *pConfigFileName, ClientListObject_t *pClientList, ProcedureListObject_t *pProcedureList, ServoControllerObject_t *pServoControllerData)
